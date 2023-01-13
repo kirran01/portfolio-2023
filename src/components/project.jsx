2b79ab35cfa009564bcbde16b2ebe694825e6d8f
kirran01 <Kirran-@live.com>
2023-01-13 16:06:43 -0400

    project cards

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link, Router } from 'react-router-dom'
 
 
-const Project = () => {
+const Project = ({ project }) => {
     return (
-        <div>
-            <h2>title</h2>
+        <div className='flex flex-col text-center items-center bg-white p-10 pt-2 pb-2 m-3 rounded-lg'>
+            <h1 className='text-lg'>{project.title}</h1>
             <hr />
-            <img src="" alt="img" />
-            <h4>stack caption</h4>
-            <button>Demo</button>
-            <Link to="www.google.com">repo</Link>
+            <img className='h-16 my-5' src={project.image} alt="" />
+            <p className='text-sm my-2'>{project.stack}</p>
+            <button className='bg-slate-700 text-white m-2 px-5 py-2 rounded-lg'>Demo</button>
+            <Link className='text-sm m-5' to="www.google.com">Github Repository</Link>
         </div>
     );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,22 +6,22 @@ const Home = () => {
     const projs = [
         {
             title: 'JS Brickbreaker',
-            stack:'HTML5, CSS3, Javascript',
-            image: '../../public/p1 copy.png',
+            stack: 'HTML5, CSS3, Javascript',
+            image: '../../public/CONTROLLER.png',
             repo: 'https://github.com/kirran01/Brickbreaker',
             site: 'https://kirran01.github.io/Brickbreaker/'
         },
         {
             title: 'Studio Ghibli Archive',
-            stack:'HTML5, CSS3, Javascript, NodeJS, Handlebars, Express, MongoDB',
-            image: '../../public/p2 copy.png',
+            stack: 'HTML5, CSS3, Javascript, NodeJS, Handlebars, Express, MongoDB',
+            image: '../../public/TOTORO.png',
             repo: 'https://github.com/kirran01/Studio-Ghibli-Archive',
             site: 'https://ghibliarchive.fly.dev/'
         },
         {
             title: 'Task Manager',
-            stack:'ReactJS, CSS3, NodeJs, Express, MongoDB',
-            image: '../../public/p3 copy.png',
+            stack: 'ReactJS, CSS3, NodeJs, Express, MongoDB',
+            image: '../../public/TASKS.png',
             repo: 'https://github.com/kirran01/planner-frontend',
             site: 'https://task-manager-kk.netlify.app/'
         }
@@ -29,7 +29,17 @@ const Home = () => {
     return (
         <div className='bg-slate-200'>
             <h1>home</h1>
-            <Project/>
+            <div className='flex justify-center'>
+                {
+                    projs.map(proj => {
+                        return (
+                            <>
+                            <Project key={proj.title} project={proj}/>
+                            </>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
